8ff1b9e2d8de15848ffe8073916388f24f63287a
qindongfang@zhongan.com <qindongfang@zhongan.com>
2018-08-07 14:46:58 +0800

    init

---
+++ logutil/src/main/java/com/dfqin/logutil/LogUtil.java
@@ -0,0 +1,14 @@
+package com.dfqin.logutil;
+
+import android.util.Log;
+
+/**
+ * Created by dfqin on 2018/8/7.
+ */
+
+public class LogUtil {
+
+    public static void log(String log) {
+        Log.v("MyLog", log);
+    }
+}
